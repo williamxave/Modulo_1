9a981ed74a9ad42bce5d1fd98fde7de09132db9a
William bohn <williamxavero@gmail.com>
2021-10-20 22:13:14 -0300

    Refactory desafio 3

--- src/main/java/modulo1/desafio3/main.java
+++ src/main/java/modulo1/desafio3/main.java
@@ -7,17 +7,48 @@ public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        var valorDaProva = 0;
+        var media = 0;
+        var validaMedia = 0;
+        var validaProva = 0;
+
         System.out.print("Digite o valor de uma das provas: ");
-        var valorDaProva = sc.nextInt();
+        valorDaProva = sc.nextInt();
+        validaProva = validaProva(valorDaProva, sc);
 
         System.out.print("Digite a média: ");
-        var media = sc.nextInt();
+        media = sc.nextInt();
+        validaMedia = validaMedia(media, sc,validaProva);
 
-        for(int i = 0; i <= 100; i++){
-            if((valorDaProva + i) / 2 == media){
-                System.out.println("Valor da segunda prova é: " + i );
-                break;
+        var valorDaPrimeiraProva = verificaMedia(validaMedia,validaProva);
+        System.out.println("Valor da segunda prova é: " + valorDaPrimeiraProva);
+
+    }
+
+    private static Integer validaProva(Integer primeraProva, Scanner sc) {
+        if (primeraProva < 0 || primeraProva > 100) {
+            while (primeraProva < 0 || primeraProva > 100) {
+                System.out.print("Digite um valor válido para a prova: ");
+                primeraProva = sc.nextInt();
             }
         }
+        return primeraProva;
     }
+
+    private static Integer validaMedia(Integer media, Scanner sc, Integer valorProva) {
+        var valorDaPrimeiraProva = verificaMedia(media,valorProva);
+        if(media < 0 || media > 100 || valorDaPrimeiraProva > 100){
+            while (media < 0 || media > 100 || valorDaPrimeiraProva > 100) {
+                System.out.print("Digite um valor válido para a média: ");
+                media = sc.nextInt();
+            }
+        }
+        return media;
+    }
+
+    private static  Integer verificaMedia(Integer media, Integer valorProva){
+        var valorDaPrimeiraProva = (media * 2) - valorProva;
+        return valorDaPrimeiraProva;
+    }
+
 }
